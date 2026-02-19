c4ac854b4fd6b3e76a1e2cf3ce1a2ec8bbc38529
WalterDiFrancesco <134644270+webpages123@users.noreply.github.com>
2026-02-19 16:41:26 -0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://launicafutbol550-stack.github.io/launicafutbol550-stack/'
 });
